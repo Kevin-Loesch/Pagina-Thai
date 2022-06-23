80b43d87653fa5619dc18cde3868dc56bff5f616
Kevin Loesch <100306376+Kevin-Loesch@users.noreply.github.com>
2022-06-23 15:06:03 -0300

    .22

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 import Countdown from "./countdown.js";
 
-const tempoRestante = new Countdown("29 juni 2022 09:00:00 GMT-0300");
+const tempoRestante = new Countdown("23 juni 2022 22:00:00 GMT-0300");
 const tempos = document.querySelectorAll("[data-time]");
 
 function Mostrartempo() {
